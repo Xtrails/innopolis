4893977c733f32284bbc0ce6a56b3e3239b123f1
Pavel Anisimov <anisimov.pa@udmonline.ru>
2019-05-13 10:21:31 +0400

    task01

---
+++ part01/lesson08/task01/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Урок 7. Задание 1.
+ *
+ * Необходимо разработать класс, реализующий следующие методы:
+ * void serialize (Object object, String file);
+ * Object deSerialize(String file);
+ *
+ * @version   1.0 13.05.2019
+ * @author    Pavel Anisimov
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Home home = new Home("Truda 2");
+        SerializableMgr.serialize(home,"home.txt");
+        Home home2 = (Home) SerializableMgr.deSerialize("home.txt");
+        System.out.println(home2.getHome());
+    }
+}
